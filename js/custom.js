a10acd2c87fe457a678e3379d52c41f916c0d80b
Scott Conway <conway.sc@husy.neu.edu>
2014-12-28 16:53:24 -0500

    adding backround images and navigation animations

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,46 @@
 
-function createTimeout($el, delay) {
+function addAfterTimeout($el, className, delay) {
   setTimeout(function() {
-    $el.addClass("in-place");
-  }, delay)
+    $el.addClass(className);
+  }, delay);
+}
+
+function coverContainer(delay) {
+  setTimeout(function() {
+    $(".overlay").show(1000);
+  }, delay);
 }
 
 $(document).ready(function() {
 
-    createTimeout($(".piece1"), 50);
-    createTimeout($(".piece2"), 1000);
-    createTimeout($(".piece4"), 1500);
-    createTimeout($(".piece5"), 2000);
-    createTimeout($(".piece3"), 2500);
-    createTimeout($(".piece6"), 4500);
-    createTimeout($(".piece7"), 3500);
-    createTimeout($(".piece8"), 4000);
-    createTimeout($(".piece9"), 3000);
-    createTimeout($(".piece10"), 8000);
-    createTimeout($(".piece11"), 5500);
-    createTimeout($(".piece12"), 7000);
-    createTimeout($(".piece13"), 6500);
-    createTimeout($(".piece14"), 6000);
-    createTimeout($(".piece15"), 7500);
-    createTimeout($(".piece16"), 5000);
+    if ($(window).width() < 768) {
+        addAfterTimeout($(".piece1"), "in-place", 50);
+        addAfterTimeout($(".piece4"), "in-place", 1500);
+        addAfterTimeout($(".piece13"), "in-place", 3500);
+        addAfterTimeout($(".piece16"), "in-place", 2500);
+        coverContainer(5000);
+        addAfterTimeout($(".home-nav"), "in-place", 6000);
+    }else{
+        addAfterTimeout($(".piece1"), "in-place", 50);
+        addAfterTimeout($(".piece2"), "in-place", 1000);
+        addAfterTimeout($(".piece4"), "in-place", 1500);
+        addAfterTimeout($(".piece5"), "in-place", 2000);
+        addAfterTimeout($(".piece3"), "in-place", 2500);
+        addAfterTimeout($(".piece6"), "in-place", 4500);
+        addAfterTimeout($(".piece7"), "in-place", 3500);
+        addAfterTimeout($(".piece8"), "in-place", 4000);
+        addAfterTimeout($(".piece9"), "in-place", 3000);
+        addAfterTimeout($(".piece10"), "in-place", 8000);
+        addAfterTimeout($(".piece11"), "in-place", 5500);
+        addAfterTimeout($(".piece12"), "in-place", 7000);
+        addAfterTimeout($(".piece13"), "in-place", 6500);
+        addAfterTimeout($(".piece14"), "in-place", 6000);
+        addAfterTimeout($(".piece15"), "in-place", 7500);
+        addAfterTimeout($(".piece16"), "in-place", 5000);
+        coverContainer(9500);
+        addAfterTimeout($(".home-nav"), "in-place", 10500);
+    }
+
+    
 
 });
